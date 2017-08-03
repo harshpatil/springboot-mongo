7ba1fecde132856172d7138e95a5f0f1ac1dfae0
Harshavardhan Patil <patil.bmsce@gmail.com>
2017-08-02 19:54:19 -0500

    Removed swagger deprecated apiinfo method

--- src/main/java/com/mongo/passion/config/SwaggerConfig.java
+++ src/main/java/com/mongo/passion/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,19 +19,6 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    @SuppressWarnings("deprecation")
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "API DOC",
-                "Self explanatory API documenation",
-                "API TOS",
-                "harshpatil.github.io",
-                "Harsh Patil",
-                "harshpatil.github.io", null
-        );
+                .build();
     }
 }
